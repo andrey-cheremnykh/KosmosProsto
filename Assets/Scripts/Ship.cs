597b816bd41dfd2b5ece2ed93209a47d057d5851
Andrey <Andrey@DESKTOP-NI1F48K>
2023-02-21 21:15:05 +0200

    Project+Ship Movement

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+
+    [SerializeField]Transform camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        float distance = Mathf.Infinity;
+        RaycastHit hitInfo; 
+        bool isHit = Physics.Raycast(camera.transform.position, transform.TransformDirection(Vector3.forward), out hitInfo, distance);
+            Debug.DrawRay(camera.transform.position, transform.TransformDirection(Vector3.forward)*1000, Color.green,3f);
+        if (isHit)
+        {
+            print(hitInfo.transform.position);
+
+        }
+        else
+        {
+            print("aboba");
+        }
+    }
+}
